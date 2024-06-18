738f31ad5a09d0c31372275ce95889f05e143f6a
Vivan-verma1109 <108888443+Vivan-verma1109@users.noreply.github.com>
2024-06-18 12:59:03 -0700

    clean up

--- LinkedListTest.java
+++ LinkedListTest.java
@@ -1,12 +1,4 @@
-// Virginia Tech Honor Code Pledge:
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those who
-// do.
-// -- Darren Hsu (darrenhsu)
-// -- Nami Jain (jainnami)
-// -- Sam Shumaker (sshumak)
-// -- Vivan Verma (vivanv)
+
 package prj5;
 
 import java.util.Arrays;
@@ -15,10 +7,7 @@ import java.util.Comparator;
 /**
  * Tests Methods in the LinkedList class
  * 
- * @author Darren Hsu (darrenshu)
- * @author Nami Jain (jainnami)
- * @author Sam Shumaker (sshumak)
- * @author Vivan Verma (vivanv)
+
  * @version 12/01/2023
  */
 public class LinkedListTest extends student.TestCase
