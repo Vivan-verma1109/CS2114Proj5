df1c32727acbe50a83e545c59dc000aedb57d7c4
Vivan-verma1109 <108888443+Vivan-verma1109@users.noreply.github.com>
2024-06-18 12:56:05 -0700

    clean ComparatorEngagementReach.java

--- ComparatorEngagementReach.java
+++ ComparatorEngagementReach.java
@@ -1,12 +1,3 @@
-// Virginia Tech Honor Code Pledge:
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those who
-// do.
-// -- Darren Hsu (darrenhsu)
-// -- Nami Jain (jainnami)
-// -- Sam Shumaker (sshumak)
-// -- Vivan Verma (vivanv)
 package prj5;
 
 import java.util.Comparator;
