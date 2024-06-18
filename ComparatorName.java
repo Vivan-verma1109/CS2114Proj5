f6ce433cbfbd17e2dc1c729948385cd7841008fc
Vivan-verma1109 <108888443+Vivan-verma1109@users.noreply.github.com>
2024-06-18 12:57:29 -0700

    clean up

--- ComparatorName.java
+++ ComparatorName.java
@@ -1,13 +1,4 @@
-// Virginia Tech Honor Code Pledge:
 
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those who
-// do.
-// -- Darren Hsu (darrenhsu)
-// -- Nami Jain (jainnami)
-// -- Sam Shumaker (sshumak)
-// -- Vivan Verma (vivanv)
 package prj5;
 
 import java.util.Comparator;
@@ -15,10 +6,7 @@ import java.util.Comparator;
 /**
  * Compares Influencers alphabetically based on Channel Name
  * 
- * @author Darren Hsu (darrenshu)
- * @author Nami Jain (jainnami)
- * @author Sam Shumaker (sshumak)
- * @author Vivan Verma (vivanv)
+
  * @version 12/01/2023
  */
 public class ComparatorName implements Comparator<Influencer>
