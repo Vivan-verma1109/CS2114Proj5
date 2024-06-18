f37571578d5d209b520c3062e252f20f36f1be9d
Vivan-verma1109 <108888443+Vivan-verma1109@users.noreply.github.com>
2024-06-18 12:58:43 -0700

    clean up

--- InputFileReader.java
+++ InputFileReader.java
@@ -1,12 +1,4 @@
-// Virginia Tech Honor Code Pledge:
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those who
-// do.
-// -- Darren Hsu (darrenhsu)
-// -- Nami Jain (jainnami)
-// -- Sam Shumaker (sshumak)
-// -- Vivan Verma (vivanv)
+
 package prj5;
 
 import java.io.File;
@@ -16,10 +8,7 @@ import java.util.Scanner;
 /**
  * Reads data from files and generates Influencers
  * 
- * @author Darren Hsu (darrenshu)
- * @author Nami Jain (jainnami)
- * @author Sam Shumaker (sshumak)
- * @author Vivan Verma (vivanv)
+
  * @version 12/01/2023
  */
 public class InputFileReader
